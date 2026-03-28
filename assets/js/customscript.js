490a08e1c796c19f1b4267a076323e59033f5637
Ananthu.c <ananthu.c.666@gmail.com>
2026-03-28 08:32:51 +0530

    inital commit

--- assets/js/customscript.js
+++ assets/js/customscript.js
@@ -26,123 +26,56 @@ $(function () {
       $("header").removeClass("effect");
     }
   });
+});
 
-  // Slider
-  const slideCount = $(".ku-slider__item").length;
-  const indexCount = slideCount - 1;
-
-  // Build single progress bar + numbers
-  $(".ku-slider__progress").html(`
-    <span class="slide-current">1</span>
-    <div class="progressBar"><div class="inProgress"></div></div>
-    <span class="slide-total">${slideCount}</span>
-`);
-
-  // Init slick
-  $(".ku-slider").slick({
-    infinite: true,
-    arrows: false,
-    dots: false,
-    autoplay: false,
-    speed: 600,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    cssEase: "ease-in-out",
+document.addEventListener("DOMContentLoaded", function () {
+  //Nav Menu
+  const menuButton = document.querySelector(".custom-ham");
+  const mobileMenu = document.querySelector(".ma-nav__mobile-menu");
+  const links = document.querySelector(".ma-nav__links");
+  const btn = document.querySelector(".ma__btn");
+  menuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
   });
-
-  // Progress system
-  let percentTime;
-  let tick;
-  let time = 0.1;
-  let progressBarIndex = 0;
-
-  function startProgressbar() {
-    resetProgressbar();
-    percentTime = 0;
-    tick = setInterval(interval, 10);
-  }
-
-  function interval() {
-    const currentSlide = $(".ku-slider .slick-track .slick-active").data("slickIndex");
-
-    $(".slide-current").text(currentSlide + 1); // Update number
-
-    if (currentSlide !== progressBarIndex) {
-      progressBarIndex = currentSlide;
-      startProgressbar();
-    } else {
-      percentTime += 1 / (time + 5);
-      $(".inProgress").css({ width: percentTime + "%" });
-
-      if (percentTime >= 100) {
-        $(".ku-slider").slick("slickNext");
-        progressBarIndex++;
-        if (progressBarIndex > indexCount) progressBarIndex = 0;
-        startProgressbar();
-      }
-    }
-  }
-
-  function resetProgressbar() {
-    $(".inProgress").css({ width: "0%" });
-    clearInterval(tick);
-  }
-
-  startProgressbar();
-
-  //news slider
-  $(".ku-news__slide").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true,
-    appendArrows: ".ku-news__slidebtn",
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-
-  //events slider
-  $(".ku-events__slide").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true,
-    appendArrows: ".ku-events__slidebtn",
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-
   //Wow js
   new WOW().init();
+
+  //slider
+  $(".ma-clients__slider").slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 10,
+    cssEase: "linear",
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
